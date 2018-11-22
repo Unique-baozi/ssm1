230079cd64f746570dec0a2a306be10f3bbf0676
unique <123@qq.com>
2018-11-22 21:00:41 +0800

    v3

---
+++ Test01.java
@@ -0,0 +1,16 @@
+package cn.itheima.practice;
+
+public class Test01 {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50,60,66,70,80,99};
+        printArray(arr);
+    }
+    public static void printArray(int[] arr){
+        System.out.println("您的大乐透的号码为：");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+
+}
